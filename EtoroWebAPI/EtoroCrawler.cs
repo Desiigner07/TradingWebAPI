0ecc9688040b3b09f6069edf1a3cf3226cb0790a
Pierre Pohly <pohly@noxum.com>
2018-06-27 08:43:21 +0200

    Initial commit

---
+++ EtoroWebAPI/EtoroCrawler.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using OpenQA.Selenium.IE;
+using OpenQA.Selenium.Interactions;
+
+namespace EtoroWebAPI
+{
+	public class EtoroCrawler
+	{
+		private const string ETORO_URL = "https://www.etoro.com/";
+
+		#region Properties 
+
+		public InternetExplorerDriver Driver { get; private set; }
+
+		#endregion
+
+
+		private void StartBrowser()
+		{
+			this.Driver = new InternetExplorerDriver();
+			this.Driver.Url = ETORO_URL;
+		}
+		
+		public void Login(string username, string pwd)
+		{
+			IWebElement loginElement = this.Driver.FindElementByClassName("login");
+			loginElement.Click();
+
+			IWebElement usernameBox = this.Driver.FindElementById("username");
+			IWebElement pwdBox = this.Driver.FindElementById("password");
+
+			usernameBox.SendKeys(username);
+			pwdBox.SendKeys(pwd);
+
+			IWebElement loginButtonElement = this.Driver.FindElementById("login-sts-btn-sign-in");
+			loginButtonElement.Click();
+		}
+
+	}
+}

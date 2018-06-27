7fab4af6aade4fccd25f81dded36ec5b090c6058
Pierre Pohly <pohly@noxum.com>
2018-06-27 09:31:06 +0200

    Add functions

--- EtoroWebAPI/EtoroCrawler.cs
+++ EtoroWebAPI/EtoroCrawler.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using OpenQA.Selenium;
-using OpenQA.Selenium.IE;
+using OpenQA.Selenium.Firefox;
 using OpenQA.Selenium.Interactions;
 
 namespace EtoroWebAPI
@@ -15,31 +15,97 @@ namespace EtoroWebAPI
 
 		#region Properties 
 
-		public InternetExplorerDriver Driver { get; private set; }
+		public IWebDriver Driver { get; private set; }
+
+		#region Menu WebElements
+
+		private IWebElement _watchlistElement;
+		public IWebElement WatchlistElement
+		{
+			get
+			{
+				if (_watchlistElement == null)
+				{
+					_watchlistElement = this.Driver.FindElements(By.ClassName("i-menu-link")).First(e => e.GetAttribute("ng-href") == "/watchlists");
+				}
+				return _watchlistElement;
+			}
+		}
+
+		private IWebElement _portfolioElement;
+		public IWebElement PortfolioElement
+		{
+			get
+			{
+				if (_portfolioElement == null)
+				{
+					_portfolioElement = this.Driver.FindElements(By.ClassName("i-menu-link")).First(e => e.GetAttribute("ng-href") == "/app/portfolio-page");
+				}
+				return _portfolioElement;
+			}
+		}
+
+
+		#endregion
 
 		#endregion
 
 
 		private void StartBrowser()
 		{
-			this.Driver = new InternetExplorerDriver();
+			this.Driver = new FirefoxDriver();
 			this.Driver.Url = ETORO_URL;
 		}
-		
+
 		public void Login(string username, string pwd)
 		{
-			IWebElement loginElement = this.Driver.FindElementByClassName("login");
+			IWebElement loginElement = this.Driver.FindElement(By.ClassName("login"));
 			loginElement.Click();
 
-			IWebElement usernameBox = this.Driver.FindElementById("username");
-			IWebElement pwdBox = this.Driver.FindElementById("password");
+			IWebElement usernameBox = this.Driver.FindElement(By.Id("username"));
+			IWebElement pwdBox = this.Driver.FindElement(By.Id("password"));
 
 			usernameBox.SendKeys(username);
 			pwdBox.SendKeys(pwd);
 
-			IWebElement loginButtonElement = this.Driver.FindElementById("login-sts-btn-sign-in");
+			IWebElement loginButtonElement = this.Driver.FindElement(By.Id("login-sts-btn-sign-in"));
 			loginButtonElement.Click();
 		}
 
+		public void OpenVirtualMode()
+		{
+			IWebElement dropdownElement = this.Driver.FindElement(By.ClassName("i-menu-link-mode-demo dropdown-menu ng-binding ng-scope"));
+			dropdownElement.Click();
+
+			IWebElement virtItemElement = this.Driver.FindElement(By.ClassName("drop-select-box-option"));
+			virtItemElement.Click();
+
+			IWebElement goToVirtButton = this.Driver.FindElement(By.ClassName("w-sm-footer-button e-btn-big dark ng-binding"));
+			goToVirtButton.Click();
+		}
+
+		public void GoToWatchlist()
+		{
+			if (this.WatchlistElement != null)
+			{
+				this.WatchlistElement.Click();
+			}
+			else
+			{
+				this.Driver.Url = "https://www.Etoro.com/watchlists";
+			}
+		}
+
+		public void GoToPortfolio()
+		{
+			if (this.PortfolioElement != null)
+			{
+				this.PortfolioElement.Click();
+			}
+			else
+			{
+				this.Driver.Url = "https://www.etoro.com/portfolio";
+			}
+		}
 	}
 }

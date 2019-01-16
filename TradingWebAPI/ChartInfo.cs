472c6624141420e9057efbefc4480388047c7e10
Pierre Pohly <pohly@noxum.com>
2019-01-16 18:51:17 +0100

    Add Interface for ChartProvider

---
+++ TradingWebAPI/ChartInfo.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TradingWebAPI
+{
+    public class ChartInfo
+    {
+        public float CurrentPrice { get; private set; }
+
+        public float High { get; private set; }
+
+        public float Low { get; private set; }
+
+        public float Open { get; private set; }
+
+        public float Volume { get; private set; }
+
+        public float Change { get; private set; }
+    }
+}
---
+++ TradingWebAPI/Interfaces/IChartProvider.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TradingWebAPI.Interfaces
+{
+    public interface IChartProvider
+    {
+        ChartInfo GetCurrentChartInfo();
+
+        ChartInfo[] GetChart(int count, Delay delay);
+        
+    }
+}
